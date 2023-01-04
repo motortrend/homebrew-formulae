705ac1e5b6dbe1845d2cfdb38ad07a34442fc2de
Dmitriy Zakharov <Dmitriy_Zakharov@mototrend.com>
2023-01-03 16:32:12 -0800

    update links to files

--- Formula/gyb.rb
+++ Formula/gyb.rb
@@ -5,14 +5,14 @@
 class Gyb < Formula
   desc "\"Generate Your Boilerplate\""
   homepage "https://motortrendondemand.com/"
-  url "https://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb?token=GHSAT0AAAAAAB5AYHPL5S4E7G2SHANSIUTKY5UYQBA"
+  url "hhttps://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb"
   version "2023-01-03"
   sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
 
   depends_on xcode: ["11.0", :build]
 
   resource "gyb.py" do
-    url "https://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb.py?token=GHSAT0AAAAAAB5AYHPKJKFVUINMGWFVE7GWY5UYSHA"
+    url "https://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb.py"
     version "2023-01-03"
     sha256 "5647355be7d91007cc4c849da79bea235a7d7f97871384f0d12f639402d53190"
   end
