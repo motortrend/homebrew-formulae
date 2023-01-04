60000f461bbe53831937d0ce58bb0d21c3fdc541
Dmitriy Zakharov <Dmitriy_Zakharov@mototrend.com>
2023-01-03 16:50:59 -0800

    fix sha256

--- Formula/gyb.rb
+++ Formula/gyb.rb
@@ -7,7 +7,7 @@ class Gyb < Formula
   homepage "https://motortrendondemand.com/"
   url "https://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb"
   version "2023-01-03"
-  sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
+  sha256 "5647355be7d91007cc4c849da79bea235a7d7f97871384f0d12f639402d53190"
   
 
   depends_on xcode: ["11.0", :build]
