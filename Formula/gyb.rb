821ace3a76cc00c0550d955f32373657d36b8efe
Dmitriy Zakharov <Dmitriy_Zakharov@mototrend.com>
2023-01-03 16:37:59 -0800

    update sha256

--- Formula/gyb.rb
+++ Formula/gyb.rb
@@ -8,13 +8,14 @@ class Gyb < Formula
   url "hhttps://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb"
   version "2023-01-03"
   sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
+  
 
   depends_on xcode: ["11.0", :build]
 
   resource "gyb.py" do
     url "https://raw.githubusercontent.com/motortrend/homebrew-formulae/main/gyb/gyb.py"
     version "2023-01-03"
-    sha256 "5647355be7d91007cc4c849da79bea235a7d7f97871384f0d12f639402d53190"
+    sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
   end
   depends_on "python@3" if MacOS.version <= :snow_leopard
 
